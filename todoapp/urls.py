550f36c6922dfd37dbf7ed33a2f15ce45f66b45b
Purum <y_purum@naver.com>
2021-02-24 23:47:11 +0900

    add search

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,12 +2,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from django.urls import path, include
 from .views import TodoList, TodoDetail, UserTodoList
-
+# from .views import Search
 
 urlpatterns = [
     path('list/', TodoList.as_view()),
-    path('list/detail/<int:pk>/', TodoDetail.as_view()),
+    path('list/<int:pk>/', TodoList.as_view()),
+    path('detail/<int:pk>/', TodoDetail.as_view()),
     path('user/<int:pk>/', UserTodoList.as_view()),
+    # path('search/', Search_as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
